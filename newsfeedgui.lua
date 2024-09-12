2a9529de473b09717f82b75fe8e8f5013c8ee7ef
Lone Mauc <33334792+LMauc@users.noreply.github.com>
2024-09-12 13:23:00 -0300

    Disable Feed

---
+++ newsfeedgui.lua
@@ -0,0 +1,32 @@
+NewsFeedGui = NewsFeedGui or class(TextBoxGui)
+NewsFeedGui.PRESENT_TIME = 0
+NewsFeedGui.SUSTAIN_TIME = 0
+NewsFeedGui.REMOVE_TIME = 0
+NewsFeedGui.MAX_NEWS = 0
+
+function NewsFeedGui:init()
+end
+
+function NewsFeedGui:update()
+end
+
+function NewsFeedGui:make_news_request()
+end
+
+function NewsFeedGui:news_result()
+end
+
+function NewsFeedGui:_create_gui()
+end
+
+function NewsFeedGui:_get_text_block()
+end
+
+function NewsFeedGui:mouse_moved()
+end
+
+function NewsFeedGui:mouse_pressed()
+end
+
+function NewsFeedGui:close()
+end
